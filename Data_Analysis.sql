e9417f7d26046ed3b2ce43f32b5678337d996926
Dinmaharbs <chidinmaharbor@gmail.com>
2024-08-22 21:37:28 +0100

    Update Data_Analysis.sql
    added code for
    1 total number of trips
    2 number of trips by start station
    3 number of trips by rideable type

--- Data_Analysis.sql
+++ Data_Analysis.sql
@@ -1,15 +1,41 @@
+-- Total number of trips by members
+SELECT
+  COUNT(ride_id) AS num_of_trips,
+  member_casual
+FROM `project-practice-417813.cyclistic_case_study.cyclistic-tripdata`
+GROUP BY member_casual
+  
 --Average trip duration grouped by member type
 SELECT 
   AVG(trip_duration_min) AS avg_trip_duration,
   member_casual
-FROM `project-practice-417813.cyclistic_case_study.Q1-2024-divvy-tripdata-V3`
+FROM `project-practice-417813.cyclistic_case_study.cyclistic-tripdata`
 GROUP BY member_casual
 
---number of trips by day of the week
+--Number of trips by day of the week
 SELECT 
   COUNT(startday_of_week) AS num_of_trips_by_day,
   member_casual,
   startday_of_week
-FROM `project-practice-417813.cyclistic_case_study.Q1-2024-divvy-tripdata-V3`
+FROM `project-practice-417813.cyclistic_case_study.cyclistic-tripdata`
 GROUP BY member_casual, startday_of_week
 ORDER BY num_of_trips_by_day DESC
+
+-- Calculate the number of trips by ridetype to find the ridetype preference of annual and casual members
+SELECT 
+  COUNT(rideable_type) AS rides_by_ridetype,
+  member_casual,
+  rideable_type
+FROM `project-practice-417813.cyclistic_case_study.cyclistic-tripdata`
+GROUP BY member_casual, rideable_type
+ORDER BY rides_by_ridetype DESC
+
+-- Number of trips by start station
+SELECT
+  COUNT(ride_id) AS num_of_trips,
+  member_casual,
+  start_station
+FROM `project-practice-417813.cyclistic_case_study.cyclistic-tripdata`
+WHERE start_station <> 'N/A'
+GROUP BY start_station, member_casual
+ORDER BY num_of_trips DESC
